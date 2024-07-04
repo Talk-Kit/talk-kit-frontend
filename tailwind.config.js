9f98b8ef6cfb99be2bba67ed0af80207c154ba09
kimgorok <kjyse2@naver.com>
2024-07-04 21:32:04 +0900

    tailwind.config.js 컬러 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          0: "#F7F7F7",
+          1: "#EAEAEA",
+          2: "#DADADA",
+          3: "#B5B5B5",
+          4: "#737373",
+          9: "#1A1A1A",
+        },
+        primary: {
+          0: "#F4F5FF",
+          1: "#6B71FF",
+        },
+        red: {
+          1: "#E61919",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
