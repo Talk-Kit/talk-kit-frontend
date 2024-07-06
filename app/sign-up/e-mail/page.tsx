e30a347c55667526633a176a63311c3c951398c7
kimgorok <kjyse2@naver.com>
2024-07-06 14:04:08 +0900

    🐢[회원가입_이메일 인증] : 이메일 입력 로직 진행

--- app/sign-up/e-mail/page.tsx
+++ app/sign-up/e-mail/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TalkKitLogo from "../../components/LOGO";
 import { useRouter } from "next/navigation";
 import SignUpProgress from "../components/SignUpProgress";
 
 export default function SignUp_Email() {
   const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+
+  useEffect(() => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsEmailValid(emailRegex.test(email)); // 정규식 검사
+  }, [email]);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleAuthRequest = () => {
+    if (isEmailValid) {
+      console.log("인증 요청");
+    }
+  };
 
   const [isButtonActive, setIsButtonActive] = useState(false);
   return (
@@ -26,10 +44,18 @@ export default function SignUp_Email() {
       {/* 이메일을 입력해주세요 */}
       <div className="flex w-full items-center max-w-[600px] py-2 px-4 h-[50px] justify-between border-[1px] border-gray-3 rounded-lg">
         <input
-          className="w-full placeholder:text-gray-3"
+          type="email"
+          className="w-full placeholder:text-gray-3 outline-none"
           placeholder="이메일을 입력해주세요"
+          value={email}
+          onChange={handleEmailChange}
         />
-        <div className="flex items-center justify-center py-2 px-3 rounded-lg border-[1px] bg-primary-1 ">
+        <div
+          onClick={handleAuthRequest}
+          className={`flex items-center justify-center py-2 px-3 rounded-lg border-[1px] cursor-pointer ${
+            isEmailValid ? "bg-primary-1" : "bg-gray-2"
+          }`}
+        >
           <span className="text-gray-0 text-xs whitespace-nowrap h-5">
             인증요청
           </span>
