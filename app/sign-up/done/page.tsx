913d3e4bd6c9f689122a123dd219b70ad8593a11
kimgorok <kjyse2@naver.com>
2024-07-10 20:58:26 +0900

    🌱[회원가입_회원생성] : 닉네임 전역변수 세팅

--- app/sign-up/done/page.tsx
+++ app/sign-up/done/page.tsx
@@ -3,14 +3,17 @@
 import { useRouter } from "next/navigation";
 import TalkKitLogo from "../../components/LOGO";
 import PrimaryButton from "../components/PrimaryButton";
+import { useRecoilValue } from "recoil";
+import { nicknameState } from "../state/atom";
 
 export default function SignUp_Done() {
   const router = useRouter();
+  const nickname = useRecoilValue(nicknameState);
   return (
     <div className="signup-screen">
       <TalkKitLogo width={280} height={79} />
       <div className="signup-max-w-600 py-6 px-0 flex-center text-center text-xl font-semibold">
-        김현중님 <br /> 회원가입을 축하드려요!
+        {nickname}님 <br /> 회원가입을 축하드려요!
       </div>
 
       <PrimaryButton
