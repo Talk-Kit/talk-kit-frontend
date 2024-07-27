9afe444374336d0538e06e10082fb1fbae23c818
seorang42 <seorang42@naver.com>
2024-07-27 23:25:50 +0900

    TopBar 컴포넌트 리팩토링

---
+++ app/_constants/constants.ts
@@ -0,0 +1,26 @@
+export const LARGE_TOPBAR_AUTH = [
+  { text: "대본작성", url: "/script" /* url 미정 */ },
+  { text: "발표연습", url: "/practice" },
+  { text: "커뮤니티", url: "/community" /* url 미정 */ },
+];
+
+export const LARGE_TOPBAR_AUTH_BTN = ["프로젝트 생성"];
+
+export const LARGE_TOPBAR_UNAUTH = [
+  { text: "로그인", url: "/sign-in" },
+  { text: "회원가입", url: "/sign-up" },
+];
+
+export const SMALL_TOPBAR_AUTH = [
+  { text: "워크스페이스", url: "/my-project" },
+  { text: "프로젝트 생성", url: "" },
+  { text: "대본작성", url: "/community" /* url 미정 */ },
+  { text: "발표연습", url: "/practice" },
+  { text: "커뮤니티", url: "/community" /* url 미정 */ },
+  { text: "설정", url: "/setting" /* url 미정 */ },
+];
+
+export const SMALL_TOPBAR_UNAUTH = [
+  { text: "로그인", url: "/sign-in" },
+  { text: "회원가입", url: "/sign-up" },
+];
